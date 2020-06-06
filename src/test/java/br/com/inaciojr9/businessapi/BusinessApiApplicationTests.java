0225dc72219caa5ae50241cb67db8b9efd10e3ed
Jose Inacio da Silva Junior <inaciojr9@gmail.com>
2020-06-05 23:03:31 -0300

    setting sts

---
+++ src/test/java/br/com/inaciojr9/businessapi/BusinessApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.inaciojr9.businessapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
